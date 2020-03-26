40cdb6eab45e10dea7c56efbb5ff18e8749b8f52
Quertte <a.injen90@gmail.com>
2020-03-26 22:14:58 +0300

    5. Реализовать класс Tracker[#254177](Подкорректировал, кое-что поправил)

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -17,7 +17,7 @@ public class Tracker {
     /**
      * Метод добавления заявки в хранилище
      */
-    public Item add(Item item){
+    public Item add(Item item) {
         item.setId(generateId());
         items[position++] = item;
         return item;
@@ -26,49 +26,40 @@ public class Tracker {
     /**
      * Метод генерирует уникальный ключ для заявки.
      * ТК у заявки нет уникальности полей, имени описания. Для идентификации нужен уникальный ключ.
+     *
      * @return Уникальный ключ
      */
 
-    private String generateId(){
+    private String generateId() {
         // Реализовать метод генерации
         Random rm = new Random();
         return String.valueOf(rm.nextLong() + System.currentTimeMillis());
     }
 
-    public Item[] findAll(){
-        Item[] nameWithOutNull = new Item[this.items.length];
-        int size = 0;
-        for(int i = 0; i < position; i ++ ){
-            Item it = this.items[i];
-            if(it != null){
-                nameWithOutNull[size] = it;
-                size++;
-            }
-        }
-        nameWithOutNull = Arrays.copyOf(nameWithOutNull,size);
-        return nameWithOutNull;
+    public Item[] findAll() {
+        return Arrays.copyOf(this.items, position);
     }
 
-    public Item[] findByName(String key){
+    public Item[] findByName(String key) {
         Item[] fndName = new Item[this.items.length];
         int size = 0;
-        for(int i = 0; i < position; i++){
+        for (int i = 0; i < position; i++) {
             Item it = this.items[i];
-            if(it.getName().equals(key)){
-                fndName[size] = it;
-                size ++;
+            if (it.getName().equals(key)) {
+                fndName[size++] = it;
             }
         }
-        fndName = Arrays.copyOf(fndName,size);
+        fndName = Arrays.copyOf(fndName, size);
         return fndName;
     }
 
-    public Item findById(String id){
+    public Item findById(String id) {
         Item it = null;
-        for(int i = 0; i < position; i++){
-            it = this.items[i];
-            if(it.getId().equals(id))
-            break;
+        for (int i = 0; i < position; i++) {
+            if (this.items[i].getId().equals(id)){
+                it = this.items[i];
+                break;
+            }
         }
         return it;
     }
