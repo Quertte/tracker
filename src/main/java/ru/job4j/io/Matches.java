0e7495cdc2200b73b5cafefd59a0e5643bfc221f
Quertte <a.injen90@gmail.com>
2020-04-04 15:52:09 +0300

    2. Scanner и чтение числа из консоли.[#254113]Упростил

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -3,37 +3,41 @@ package ru.job4j.io;
 import java.util.Scanner;
 
 public class Matches {
+    private static int number = 11;
+    private static int count;
+    private static String act;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Игра в 11");
-        int number = 11;
         while (number > 0) {
-            System.out.println("Игрок 1 тянет спички ");
-            int select1 = Integer.valueOf(scanner.nextLine());
-            if (select1 < 4 && select1 > 0) {
-                number = number - select1;
-                System.out.println("Спичек на столе осталось " + number);
-                if (number <= 0) {
-                    System.out.println("Игрок 1 победил");
-                    break;
+            act = count % 2 == 0 ? "Первый" : "Второй";
+            System.out.println("Ходит " + act + " игрок.\nОсталось: " + number + " спичек из 11");
+            int select = Integer.valueOf(scanner.nextLine());
+            count++;
+            boolean run = true;
+            while (run) {
+                if (check(select)) {
+                    number -= select;
+                    System.out.println("На столе осталось спичек " + number);
+                    run = false;
+                    if (number <= 0) {
+                        System.out.println("Победил " + act + " игрок");
+                        break;
+                    }
+                } else {
+                    System.out.println("Игрок " + act + " должен взять от 1 до 3 спичек");
+                    select = Integer.valueOf(scanner.nextLine());
                 }
-            } else {
-                System.out.println("Игрок должен взять от 1 до 3 спичек");
-                System.out.println("Переход хода");
-            }
-            System.out.println("Игрок 2 тянет спички");
-            int select2 = Integer.valueOf(scanner.nextLine());
-            if (select2 < 4 && select2 > 0) {
-                number = number - select2;
-                System.out.println("Спичек на столе осталось " + number);
-                if (number <= 0) {
-                    System.out.println("Игрок 2 победил");
-                    break;
-                }
-            } else {
-                System.out.println("Игрок должен взять от 1 до 3 спичек");
-                System.out.println("Переход хода");
             }
         }
+    }
+
+    public static boolean check(int select) {
+        boolean result = false;
+        if (select < 4 && select > 0) {
+            result = true;
+        }
+        return result;
     }
 }
