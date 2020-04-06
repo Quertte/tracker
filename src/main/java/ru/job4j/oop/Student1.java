88b505eb12d826f4b7c1b262c34821f1ae245902
Quertte <a.injen90@gmail.com>
2020-04-06 21:59:08 +0300

    Всякте новые классы

---
+++ src/main/java/ru/job4j/oop/Student1.java
@@ -0,0 +1,23 @@
+package ru.job4j.oop;
+
+public class Student1 {
+
+    public static void main(String[] args) {
+        Student1 petya = new Student1();
+        String song = "I believe, I can fly";
+        petya.music(song);
+        petya.music(song);
+        petya.music(song);
+        petya.song();
+        petya.song();
+        petya.song();
+    }
+
+    public void music(String lyrics){
+        System.out.println("I can sign a song : " + lyrics);
+    }
+
+    public void song(){
+        System.out.println("I believe I can fly");
+    }
+}
