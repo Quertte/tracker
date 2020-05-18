60d2ad0f625a521d31f706259e26ef7ae5bdd1ec
Quertte <a.injen90@gmail.com>
2020-05-18 19:20:20 +0300

    2. Идентичные тексты. [#254314]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rs1 = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split((" "));
+        HashSet<String> check = new HashSet<>();
+        for (String orig : origin) {
+            check.add(orig);
+        }
+
+        for (String tex : text) {
+            if (check.contains(tex)) {
+                break;
+            }
+            rs1 = false;
+        }
+        return rs1;
+    }
+}
