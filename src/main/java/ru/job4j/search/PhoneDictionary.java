e4fda421ce943aba2112f8d4b33eb900b25e1c3b
Quertte <a.injen90@gmail.com>
2020-06-17 19:07:22 +0300

    6. Функции высшего порядка [#254074]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,7 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
 
@@ -17,12 +18,21 @@ public class PhoneDictionary {
      */
 
     public ArrayList<Person> find(String key) {
+        Predicate<Person> predicateSurname = person -> person.getSurname().contains(key);
+        Predicate<Person> predicateName = person -> person.getName().contains(key);
+        Predicate<Person> predicateAddress = person -> person.getAddress().contains(key);
+        Predicate<Person> predicatePhone = person -> person.getPhone().contains(key);
+        Predicate<Person> combine = predicateSurname.or(predicateName).or(predicateAddress).or(predicatePhone);
+
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person.getSurname().contains(key) || person.getName().contains(key)
-                    || person.getAddress().contains(key) || person.getPhone().contains(key)) {
+            if (combine.test(person)) {
                 result.add(person);
             }
+//            if (person.getSurname().contains(key) || person.getName().contains(key)
+//                    || person.getAddress().contains(key) || person.getPhone().contains(key)) {
+//                result.add(person);
+//            }
         }
         return result;
     }
--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -11,7 +11,7 @@ public class PhoneDictionaryTest {
     public void whenFindByName(){
         PhoneDictionary phones = new PhoneDictionary();
         phones.add(new Person("Petr","Arsentev","534872","Bryansk"));
-        ArrayList<Person> persons = phones.find("534");
-        assertThat(persons.get(0).getName(),is("Petr"));
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname(),is("Arsentev"));
     }
 }
